772035386f73cef110e407c8f47c9f05a284ba20
IonutRazvanCopos <130033085+IonutRazvanCopos@users.noreply.github.com>
2024-06-12 19:14:24 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let words = new Set();
+
+function addWord() {
+    const input = document.getElementById("searchInput");
+    const word = input.value.trim();
+    const errorElement = document.getElementById("error");
+
+    if (word === "") {
+        errorElement.textContent = "You should add a word in dictionary.";
+        return;
+    }
+
+    if (words.has(word)) {
+        errorElement.textContent = "This word already exists in dictionary.";
+        return;
+    }
+
+    words.add(word);
+    updateWordList();
+    input.value = "";
+    errorElement.textContent = "You have successfully added a new word.";
+}
+
+function updateWordList() {
+    wordList.innerHTML = "";
+}
+
+document.getElementById("searchInput").addEventListener("keydown", function(event) {
+    if (event.key === "Enter" || event.keyCode === 13) {
+        addWord();
+    }
+});
